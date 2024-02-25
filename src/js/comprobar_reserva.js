5b1aea9e3df4324e38cac9448abcfc89f129b40b
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-02-25 13:46:26 +0000

    modificada lógica en comprobar reserva

--- src/js/comprobar_reserva.js
+++ src/js/comprobar_reserva.js
@@ -26,7 +26,9 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        if ((selectElement.selectedIndex > 0 && correoInput.value.trim() !== '') || numeroInput.value.trim() !== '') {
+        if (selectElement.selectedIndex > 0 && (correoInput.value.trim() !== '' || numeroInput.value.trim() !== '')) {
+            // haz un salto a otra página
+            window.location.href = 'detalles_reserva.html';
         } else {
             alert('Debes seleccionar el tipo de ticket y llenar al menos uno de los campos (correo o número).');
         }
