7233c4e37de6322379b266be2f5d4f0f967f7379
Trepuco03 <106118289+Trepuco03@users.noreply.github.com>
2024-02-27 11:40:43 +0000

    Hecho prmociones componente

--- src/js/promociones.js
+++ src/js/promociones.js
@@ -9,8 +9,8 @@ const promocionesEspeciales = [
 ];
 
 const proximasPromociones = [
-    { img: "../src/assets/img/menu-3.png", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
-    { img: "../src/assets/img/menu-4.png", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
+    { img: "../src/assets/img/promocion1.jpg", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
+    { img: "../src/assets/img/promocion2.jpg", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
 ];
 
 function cargarPromociones(rol) {
@@ -18,78 +18,120 @@ function cargarPromociones(rol) {
     contenedor.innerHTML = '';
 
     promociones.forEach(promocion => {
-        const promocionesHTML = `
-        <div class="promocion-item">
-            <div class="promocion-img">
-            <img src="${promocion.img}" alt="${promocion.alt}">
-            <div class="overlay">
-                <button onclick="showReservar()"><i class="fas fa-shopping-cart"></i>Reservar</button>
-                <a href="${promocion.resenas}" class="button-link">
-                <i class="fas fa-search"></i> Ver detalles
-                </a>
-            </div>
-            </div>
-        </div>
-        `;
+        let promocionesHTML = '';
+
+        switch (rol) {
+            case 'admin': 
+                promocionesHTML = `
+                <div class="promocion-item">
+                    <div class="promocion-img">
+                        <img src="${promocion.img}" alt="${promocion.alt}">
+                        <div class="overlay">
+                            <button onclick=""><i class="fas fa-trash"></i>Eliminar</button>
+                            <a href="${promocion.resenas}" class="button-link">
+                                <i class="fas fa-pencil-alt"></i> Editar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                `;
+                break;
+            default:
+                promocionesHTML = `
+                <div class="promocion-item">
+                    <img src="${promocion.img}" alt="${promocion.alt}"></img>
+                </div>
+                `;
+                break;
+        }
         contenedor.innerHTML += promocionesHTML;
     });
 }
 
 
-function cargarPromocionesEspeciales() {
+
+function cargarPromocionesEspeciales(rol) {
     const contenedor = document.getElementById('type2');
+    contenedor.innerHTML = '';
 
-    if (contenedor) {
-        contenedor.innerHTML = '';
-    
-        promocionesEspeciales.forEach(promocion => {
-            const promocionesHTML = `
-            <div class="promocion-item">
-                <div class="promocion-img">
-                    <img src="${promocion.img}" alt="${promocion.alt}">
-                    <div class="overlay">
-                        <button onclick=""><i class="fas fa-shopping-cart"></i>Reservar</button>
-                        <a href="${promocion.resenas}" class="button-link">
-                        <i class="fas fa-search"></i> Ver detalles
-                        </a>
+    promocionesEspeciales.forEach(promocion => {
+        let promocionesHTML = '';   
+        switch (rol) {
+            case 'admin': 
+                promocionesHTML = `
+                <div class="promocion-item">
+                    <div class="promocion-img">
+                        <img src="${promocion.img}" alt="${promocion.alt}">
+                        <div class="overlay">
+                            <button onclick=""><i class="fas fa-trash"></i>Eliminar</button>
+                            <a href="${promocion.resenas}" class="button-link">
+                                <i class="fas fa-pencil-alt"></i> Editar
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            `;
-            contenedor.innerHTML += promocionesHTML;
-        });
-    } else {
-        return
-    }
+                `;
+                break;
+            default:
+                promocionesHTML = `
+                <div class="promocion-item">
+                    <img src="${promocion.img}" alt="${promocion.alt}"></img>
+                </div>
+                `;
+                break;
+        }
+        contenedor.innerHTML += promocionesHTML;
+    });
 }
 
-function cargarPromocionesProximas() {
+function cargarPromocionesProximas(rol) {
     const contenedor = document.getElementById('type3');
+    contenedor.innerHTML = '';
 
-    if (contenedor) {
-        contenedor.innerHTML = '';
-    
-        proximasPromociones.forEach(promocion => {
-            const promocionesHTML = `
-            <div class="promocion-item">
-                <div class="promocion-img">
-                    <img src="${promocion.img}" alt="${promocion.alt}">
-                    <div class="overlay">
-                        <button onclick="showReservar()"><i class="fas fa-shopping-cart"></i>Reservar</button>
-                        <a href="${promocion.resenas}" class="button-link">
-                        <i class="fas fa-search"></i> Ver detalles
-                        </a>
+    proximasPromociones.forEach(promocion => {
+        let promocionesHTML = '';
+
+        switch (rol) {
+            case 'admin': 
+                promocionesHTML = `
+                <div class="promocion-item">
+                    <div class="promocion-img">
+                        <img src="${promocion.img}" alt="${promocion.alt}">
+                        <div class="overlay">
+                            <button onclick=""><i class="fas fa-trash"></i>Eliminar</button>
+                            <a href="${promocion.resenas}" class="button-link">
+                                <i class="fas fa-pencil-alt"></i> Editar
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            `;
-            contenedor.innerHTML += promocionesHTML;
-        });
-    } else {
-        return
-    }
+                `;
+                break;
+            default:
+                promocionesHTML = `
+                <div class="promocion-item">
+                    <img src="${promocion.img}" alt="${promocion.alt}"></img>
+                </div>
+                `;
+                break;
+        }
+        contenedor.innerHTML += promocionesHTML;
+    });
 }
 
-document.addEventListener('DOMContentLoaded', cargarPromociones);
-document.addEventListener('DOMContentLoaded', cargarPromocionesEspeciales);
-document.addEventListener('DOMContentLoaded', cargarPromocionesProximas);
+document.addEventListener('DOMContentLoaded', function() {
+    const url = window.location.href;
+
+    if (url.includes('admin_gestionar_promociones.html')) {
+        cargarPromociones('admin');
+        cargarPromocionesEspeciales('admin');
+        cargarPromocionesProximas('admin');
+    } else {
+        cargarPromociones();
+        cargarPromocionesEspeciales();
+        cargarPromocionesProximas();
+    }
+
+
+    
+});
