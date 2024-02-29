ead4a53364e06d68b045e203c898cc2bd1d35708
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-02-29 02:51:48 +0000

    Translated all components for profilePurchases.html

--- src/js/tablaCompras.js
+++ src/js/tablaCompras.js
@@ -2,7 +2,7 @@ function loadTablaCompras() {
     fetch("../src/components/tablaCompras.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById('tabla-compras').innerHTML += data;
+            document.getElementById('purchase-history-table').innerHTML += data;
         })
         .catch(error => console.error('Error cargando los botones del summary:', error));
 }
