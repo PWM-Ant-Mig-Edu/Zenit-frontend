3ced4045a593c9c6a6a11f7acf55643323fb2f73
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-02-29 01:46:30 +0000

    Translated all Div Names for addMovie.html

--- src/js/adminBotones.js
+++ src/js/adminBotones.js
@@ -2,7 +2,7 @@ function loadAdminBotonesComponent() {
     fetch("../src/components/adminBotones.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById('admin-botones').innerHTML += data;
+            document.getElementById('admin-buttons').innerHTML += data;
         })
         .catch(error => console.error('Error al cargar los botones del admin:', error));
 }
