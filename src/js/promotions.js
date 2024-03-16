872fea3613300eb4f97b2875cfa754673fd17f54
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-03-16 01:46:26 +0000

    promociones con json dinamico

--- src/js/promotions.js
+++ src/js/promotions.js
@@ -1,134 +1,63 @@
-const promotions = [
-    { img: "../src/assets/img/menu-1.png", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
-    { img: "../src/assets/img/menu-2.png", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
-];
-
-const promocionesEspeciales = [
-    { img: "../src/assets/img/menu-3.png", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
-    { img: "../src/assets/img/menu-4.png", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
-];
-
-const proximasPromociones = [
-    { img: "../src/assets/img/promocion1.jpg", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
-    { img: "../src/assets/img/promocion2.jpg", alt: "promocion1", resenas: "../public/admin_anadir_promociones.html" },
-];
-
-function cargarPromociones(rol) {
-    const contenedor = document.getElementById('type1');
-    contenedor.innerHTML = '';
-
-    promotions.forEach(promocion => {
-        let promocionesHTML = '';
-
-        switch (rol) {
-            case 'admin': 
-                promocionesHTML = `
-                <div class="promocion-item">
-                    <div class="promocion-img">
-                        <img src="${promocion.img}" alt="${promocion.alt}">
-                        <div class="overlay">
-                            <button onclick=""><i class="fas fa-trash"></i>Eliminar</button>
-                            <a href="${promocion.resenas}" class="button-link">
-                                <i class="fas fa-pencil-alt"></i> Editar
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                `;
-                break;
-            default:
-                promocionesHTML = `
-                <div class="promocion-item">
-                    <img src="${promocion.img}" alt="${promocion.alt}"></img>
-                </div>
-                `;
-                break;
-        }
-        contenedor.innerHTML += promocionesHTML;
-    });
-}
-
-
-
-function cargarPromocionesEspeciales(rol) {
-    const contenedor = document.getElementById('type2');
-    contenedor.innerHTML = '';
-
-    promocionesEspeciales.forEach(promocion => {
-        let promocionesHTML = '';   
-        switch (rol) {
-            case 'admin': 
-                promocionesHTML = `
-                <div class="promocion-item">
-                    <div class="promocion-img">
-                        <img src="${promocion.img}" alt="${promocion.alt}">
-                        <div class="overlay">
-                            <button onclick=""><i class="fas fa-trash"></i>Eliminar</button>
-                            <a href="${promocion.resenas}" class="button-link">
-                                <i class="fas fa-pencil-alt"></i> Editar
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                `;
-                break;
-            default:
-                promocionesHTML = `
-                <div class="promocion-item">
-                    <img src="${promocion.img}" alt="${promocion.alt}"></img>
-                </div>
-                `;
-                break;
-        }
-        contenedor.innerHTML += promocionesHTML;
-    });
-}
-
-function cargarPromocionesProximas(rol) {
-    const contenedor = document.getElementById('type3');
-    contenedor.innerHTML = '';
-
-    proximasPromociones.forEach(promocion => {
-        let promocionesHTML = '';
-
-        switch (rol) {
-            case 'admin': 
-                promocionesHTML = `
-                <div class="promocion-item">
-                    <div class="promocion-img">
-                        <img src="${promocion.img}" alt="${promocion.alt}">
-                        <div class="overlay">
-                            <button onclick=""><i class="fas fa-trash"></i>Eliminar</button>
-                            <a href="${promocion.resenas}" class="button-link">
-                                <i class="fas fa-pencil-alt"></i> Editar
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                `;
-                break;
-            default:
-                promocionesHTML = `
-                <div class="promocion-item">
-                    <img src="${promocion.img}" alt="${promocion.alt}"></img>
-                </div>
-                `;
-                break;
-        }
-        contenedor.innerHTML += promocionesHTML;
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     const url = window.location.href;
+    const promociones = ['type1', 'type2', 'type3'];
+    const categorias = ['promotions', 'specialPromotions', 'upcomingPromotions'];
 
-    if (url.includes('adminManagePromotions.html')) {
-        cargarPromociones('admin');
-        cargarPromocionesEspeciales('admin');
-        cargarPromocionesProximas('admin');
-    } else {
-        cargarPromociones();
-        cargarPromocionesEspeciales();
-        cargarPromocionesProximas();
-    }    
+    if(url.includes('adminManagePromotions.html')) {
+        cargarPromociones('type1', 1, 'admin');
+    }
+    promociones.forEach((tipo, index) => {
+        cargarPromociones(tipo, index + 1, 'user');
+    });
 });
+
+function cargarPromociones(tipo, categoria, rol) {
+    const contenedor = document.getElementById(tipo);
+    contenedor.innerHTML = '';
+
+
+
+    fetch('../src/json/promotions.json')
+        .then(response => response.json())
+        .then(data => {
+            let dataCategory;
+            switch (categoria) {
+                case 1:
+                    dataCategory = data.promotions;
+                case 2:
+                    dataCategory = data.specialPromotions;
+                case 3:
+                    dataCategory = data.upcomingPromotions;
+            }
+            dataCategory.forEach(promocion => {
+                let promocionesHTML = '';
+
+                switch (rol) {
+                    case 'admin': 
+                        promocionesHTML = `
+                        <div class="promocion-item">
+                            <div class="promocion-img">
+                                <img src="${promocion.img}" alt="${promocion.name}">
+                                <div class="overlay">
+                                    <button onclick=""><i class="fas fa-trash"></i>Eliminar</button>
+                                    <a href="${promocion.resenas}" class="button-link">
+                                        <i class="fas fa-pencil-alt"></i> Editar
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        `;
+                        break;
+                    default:
+                        promocionesHTML = `
+                        <div class="promocion-item">
+                            <img src="${promocion.img}" alt="${promocion.name}"></img>
+                        </div>
+                        `;
+                        break;
+                }
+                contenedor.innerHTML += promocionesHTML;
+            });
+        })
+        .catch(error => console.error(`Error fetching ${categoria}:`, error));
+}
