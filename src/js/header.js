49c1e3e8f76f163aef64376b17e2b65c8c0dcd4a
EduardoCole <eduortza@gmail.com>
2024-03-22 12:49:19 +0000

    modificación header

--- src/js/header.js
+++ src/js/header.js
@@ -50,34 +50,6 @@ function loginCheck() {
     }
 }
 
-function loadHeaderComponent() {
-    fetch("../src/components/header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-component').innerHTML += data;
-        })
-        .catch(error => console.error('Error loading the header:', error));
-}
-
-document.addEventListener("DOMContentLoaded", loadHeaderComponent);
-
-
-function loadHeaderComponent() {
-    fetch("../src/components/header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-component').innerHTML += data;
-
-            // Verificar si hay datos en sessionStorage y llamar a loginCheck si es así
-            if (sessionStorage.getItem('usuario')) {
-                loginCheck();
-            }
-        })
-        .catch(error => console.error('Error loading the header:', error));
-}
-
-document.addEventListener("DOMContentLoaded", loadHeaderComponent);
-
 
 
 
