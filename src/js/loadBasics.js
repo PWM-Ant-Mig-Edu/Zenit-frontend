360db87d69444efc627f10abcf4bfc3eb8b0a645
Trepuco03 <106118289+Trepuco03@users.noreply.github.com>
2024-03-24 16:18:11 +0000

    Arreglado header (solución mala)

--- src/js/loadBasics.js
+++ src/js/loadBasics.js
@@ -1,6 +1,7 @@
-import {loadComponentJS} from './load.js';
+import { loadComponentJS } from './load.js';
 
 export function loadBasics() {
+    // Cargar los componentes básicos
     loadComponentJS('../src/components/header.html', 'header-component');
     loadComponentJS('../src/components/footer.html', 'footer-component');
     loadComponentJS('../src/components/login.html', 'login-component');
@@ -10,4 +11,47 @@ export function loadBasics() {
 
 document.addEventListener('DOMContentLoaded', function () {
     loadBasics();
+    setTimeout(checkLogin, 100);
 });
+
+function checkLogin() {
+    var  header = document.getElementById( "header-component" );
+    if (header) {
+        var isLoggedIn = sessionStorage.getItem("usuario");
+        if (isLoggedIn) {
+            var loginComponent = document.getElementById("login-component");
+            var loginImg = document.querySelector(".imagen-header");
+        
+            var loginButtonMenuContent = document.querySelector(".menu-content .button-container");
+            var loginButtonDropdown = document.querySelector(".Dropdown li");
+        
+            var popupsContainer = document.getElementById("popups");
+        
+        
+            if (loginButtonMenuContent) {
+                popupsContainer.style.display = "none";
+                loginComponent.style.display = "none";
+                loginButtonMenuContent.style.display = "none";
+                loginImg.style.display = "block";
+            }
+        
+            if (loginButtonDropdown) {
+                var liPerfil = document.getElementById("li Perfil");
+                if (liPerfil) {
+                    liPerfil.style.display = "block";
+                }
+        
+                var registerButtonDropdown = document.querySelector(".registerMobile");
+                var loginLinkDropdown = document.querySelector(".loginMobile");
+                if (registerButtonDropdown && loginLinkDropdown) {
+                    registerButtonDropdown.style.display = "none";
+                    loginLinkDropdown.style.display = "none";
+                }
+            }
+        }
+        
+    } else {
+        console.error("El componente de encabezado no se ha cargado correctamente.");
+    }
+}
+
